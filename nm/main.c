2b4ecc3d1cf2baf2be7f377bc3285eec668e0103
tatjana.galkin <tatjana.galkin@epitech.eu>
2021-03-13 21:06:51 +0100

    take away wrong error

--- nm/main.c
+++ nm/main.c
@@ -38,7 +38,8 @@ int error64(prop_t *in_file)
         fprintf(stderr, "nm: %s: file format not recognized\n", in_file->name);
         return (84);
     }
-    return error_handling64(in_file);
+    return (0);
+    //return error_handling64(in_file);
 }
 
 int error32(prop_t *f)
@@ -101,6 +102,7 @@ static int print_strerror(char *path)
 int corrupted(prop_t *in_file, size_t size)
 {
     void *end = (char *)in_file->form64.bytes + size;
+    in_file->form64.ehdr = end;
     if ((in_file->form64.ehdr = (Elf64_Ehdr *)in_file->form64.bytes) == NULL) {
         fprintf(stderr, "nm: %s: file format not recognized\n", in_file->name);
         return (84);
